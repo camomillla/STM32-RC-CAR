cf5bcb0ee9dba3fb0a8e496bb5e6bbb0d1ecbbf2
camomillla <kamila.ras10@gmail.com>
2026-01-11 04:47:40 +0100

    Created C# app

---
+++ ClientAppC/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace ClientAppC
+{
+    partial class CSApp
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonTL = new System.Windows.Forms.Button();
+            this.buttonT = new System.Windows.Forms.Button();
+            this.buttonTR = new System.Windows.Forms.Button();
+            this.buttonL = new System.Windows.Forms.Button();
+            this.buttonS = new System.Windows.Forms.Button();
+            this.buttonR = new System.Windows.Forms.Button();
+            this.buttonBL = new System.Windows.Forms.Button();
+            this.buttonB = new System.Windows.Forms.Button();
+            this.buttonBR = new System.Windows.Forms.Button();
+            this.buttonEngine = new System.Windows.Forms.Button();
+            this.buttonLights = new System.Windows.Forms.Button();
+            this.buttonHorn = new System.Windows.Forms.Button();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonTL
+            // 
+            this.buttonTL.Location = new System.Drawing.Point(237, 118);
+            this.buttonTL.Name = "buttonTL";
+            this.buttonTL.Size = new System.Drawing.Size(64, 64);
+            this.buttonTL.TabIndex = 0;
+            this.buttonTL.Text = "Top Left";
+            this.buttonTL.UseVisualStyleBackColor = true;
+            // 
+            // buttonT
+            // 
+            this.buttonT.Location = new System.Drawing.Point(307, 118);
+            this.buttonT.Name = "buttonT";
+            this.buttonT.Size = new System.Drawing.Size(64, 64);
+            this.buttonT.TabIndex = 1;
+            this.buttonT.Text = "Top";
+            this.buttonT.UseVisualStyleBackColor = true;
+            // 
+            // buttonTR
+            // 
+            this.buttonTR.Location = new System.Drawing.Point(377, 118);
+            this.buttonTR.Name = "buttonTR";
+            this.buttonTR.Size = new System.Drawing.Size(64, 64);
+            this.buttonTR.TabIndex = 2;
+            this.buttonTR.Text = "Top Right";
+            this.buttonTR.UseVisualStyleBackColor = true;
+            // 
+            // buttonL
+            // 
+            this.buttonL.Location = new System.Drawing.Point(237, 188);
+            this.buttonL.Name = "buttonL";
+            this.buttonL.Size = new System.Drawing.Size(64, 64);
+            this.buttonL.TabIndex = 3;
+            this.buttonL.Text = "Left";
+            this.buttonL.UseVisualStyleBackColor = true;
+            // 
+            // buttonS
+            // 
+            this.buttonS.Location = new System.Drawing.Point(307, 188);
+            this.buttonS.Name = "buttonS";
+            this.buttonS.Size = new System.Drawing.Size(64, 64);
+            this.buttonS.TabIndex = 4;
+            this.buttonS.Text = "Stop";
+            this.buttonS.UseVisualStyleBackColor = true;
+            // 
+            // buttonR
+            // 
+            this.buttonR.Location = new System.Drawing.Point(377, 188);
+            this.buttonR.Name = "buttonR";
+            this.buttonR.Size = new System.Drawing.Size(64, 64);
+            this.buttonR.TabIndex = 5;
+            this.buttonR.Text = "Right";
+            this.buttonR.UseVisualStyleBackColor = true;
+            // 
+            // buttonBL
+            // 
+            this.buttonBL.Location = new System.Drawing.Point(237, 258);
+            this.buttonBL.Name = "buttonBL";
+            this.buttonBL.Size = new System.Drawing.Size(64, 64);
+            this.buttonBL.TabIndex = 6;
+            this.buttonBL.Text = "Bottom Left";
+            this.buttonBL.UseVisualStyleBackColor = true;
+            // 
+            // buttonB
+            // 
+            this.buttonB.Location = new System.Drawing.Point(307, 258);
+            this.buttonB.Name = "buttonB";
+            this.buttonB.Size = new System.Drawing.Size(64, 64);
+            this.buttonB.TabIndex = 7;
+            this.buttonB.Text = "Bottom";
+            this.buttonB.UseVisualStyleBackColor = true;
+            // 
+            // buttonBR
+            // 
+            this.buttonBR.Location = new System.Drawing.Point(377, 258);
+            this.buttonBR.Name = "buttonBR";
+            this.buttonBR.Size = new System.Drawing.Size(64, 64);
+            this.buttonBR.TabIndex = 8;
+            this.buttonBR.Text = "Bottom Right";
+            this.buttonBR.UseVisualStyleBackColor = true;
+            // 
+            // buttonEngine
+            // 
+            this.buttonEngine.Location = new System.Drawing.Point(12, 118);
+            this.buttonEngine.Name = "buttonEngine";
+            this.buttonEngine.Size = new System.Drawing.Size(184, 64);
+            this.buttonEngine.TabIndex = 9;
+            this.buttonEngine.Text = "Engine";
+            this.buttonEngine.UseVisualStyleBackColor = true;
+            // 
+            // buttonLights
+            // 
+            this.buttonLights.Location = new System.Drawing.Point(12, 188);
+            this.buttonLights.Name = "buttonLights";
+            this.buttonLights.Size = new System.Drawing.Size(184, 64);
+            this.buttonLights.TabIndex = 10;
+            this.buttonLights.Text = "Lights";
+            this.buttonLights.UseVisualStyleBackColor = true;
+            // 
+            // buttonHorn
+            // 
+            this.buttonHorn.Location = new System.Drawing.Point(12, 258);
+            this.buttonHorn.Name = "buttonHorn";
+            this.buttonHorn.Size = new System.Drawing.Size(184, 64);
+            this.buttonHorn.TabIndex = 11;
+            this.buttonHorn.Text = "Horn";
+            this.buttonHorn.UseVisualStyleBackColor = true;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(12, 40);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(212, 42);
+            this.buttonConnect.TabIndex = 12;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 22);
+            this.textBox1.TabIndex = 13;
+            // 
+            // CSApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.buttonHorn);
+            this.Controls.Add(this.buttonLights);
+            this.Controls.Add(this.buttonEngine);
+            this.Controls.Add(this.buttonBR);
+            this.Controls.Add(this.buttonB);
+            this.Controls.Add(this.buttonBL);
+            this.Controls.Add(this.buttonR);
+            this.Controls.Add(this.buttonS);
+            this.Controls.Add(this.buttonL);
+            this.Controls.Add(this.buttonTR);
+            this.Controls.Add(this.buttonT);
+            this.Controls.Add(this.buttonTL);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CSApp";
+            this.Text = "C# App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonTL;
+        private System.Windows.Forms.Button buttonT;
+        private System.Windows.Forms.Button buttonTR;
+        private System.Windows.Forms.Button buttonL;
+        private System.Windows.Forms.Button buttonS;
+        private System.Windows.Forms.Button buttonR;
+        private System.Windows.Forms.Button buttonBL;
+        private System.Windows.Forms.Button buttonB;
+        private System.Windows.Forms.Button buttonBR;
+        private System.Windows.Forms.Button buttonEngine;
+        private System.Windows.Forms.Button buttonLights;
+        private System.Windows.Forms.Button buttonHorn;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ClientAppC/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientAppC
+{
+    public partial class CSApp : Form
+    {
+        private string _baseUrl;
+        private readonly HashSet<Keys> _heldArrowKeys = new HashSet<Keys>();
+        private int _lastMotor = -1;
+
+        public CSApp()
+        {
+            InitializeComponent();
+
+            KeyPreview = true;
+
+            buttonConnect.Click += (_, __) => Connect();
+            buttonEngine.Click += (_, __) => SendEndpoint("/engine", showMissingAddressMessage: true);
+            buttonHorn.Click += (_, __) => SendEndpoint("/horn", showMissingAddressMessage: true);
+            buttonLights.Click += (_, __) => SendEndpoint("/lights", showMissingAddressMessage: true);
+
+            HookMotorButton(buttonT, 2);
+            HookMotorButton(buttonTR, 3);
+            HookMotorButton(buttonR, 4);
+            HookMotorButton(buttonBR, 5);
+            HookMotorButton(buttonB, 6);
+            HookMotorButton(buttonBL, 7);
+            HookMotorButton(buttonL, 8);
+            HookMotorButton(buttonTL, 9);
+            buttonS.Click += (_, __) => SendMotor(1);
+
+            KeyDown += CSApp_KeyDown;
+            KeyUp += CSApp_KeyUp;
+            Deactivate += (_, __) => EmergencyStop();
+            FormClosing += (_, __) => EmergencyStop();
+        }
+
+        private void Connect()
+        {
+            var normalized = NormalizeBaseUrl(textBox1.Text);
+            if (string.IsNullOrWhiteSpace(normalized))
+            {
+                MessageBox.Show("Wpisz IP/adres w polu tekstowym.", "Brak adresu", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            _baseUrl = normalized;
+            SendEndpoint("/init", showMissingAddressMessage: true);
+        }
+
+        private void HookMotorButton(Button button, int motorId)
+        {
+            button.MouseDown += (_, __) => SendMotor(motorId);
+            button.MouseUp += (_, __) => SendMotor(1);
+            button.MouseLeave += (_, __) =>
+            {
+                if (Control.MouseButtons.HasFlag(MouseButtons.Left))
+                    SendMotor(1);
+            };
+        }
+
+        private void CSApp_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!IsArrowKey(e.KeyCode))
+                return;
+
+            e.Handled = true;
+            e.SuppressKeyPress = true;
+
+            if (_heldArrowKeys.Add(e.KeyCode))
+                SendMotorForHeldArrowKeys();
+        }
+
+        private void CSApp_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (!IsArrowKey(e.KeyCode))
+                return;
+
+            e.Handled = true;
+            e.SuppressKeyPress = true;
+
+            if (_heldArrowKeys.Remove(e.KeyCode))
+                SendMotorForHeldArrowKeys();
+        }
+
+        private void SendMotorForHeldArrowKeys()
+        {
+            var motor = GetMotorFromHeldArrowKeys();
+            if (motor == _lastMotor)
+                return;
+
+            _lastMotor = motor;
+            SendMotor(motor);
+        }
+
+        private int GetMotorFromHeldArrowKeys()
+        {
+            var up = _heldArrowKeys.Contains(Keys.Up);
+            var down = _heldArrowKeys.Contains(Keys.Down);
+            var left = _heldArrowKeys.Contains(Keys.Left);
+            var right = _heldArrowKeys.Contains(Keys.Right);
+
+            if (up && left) return 9;
+            if (up && right) return 3;
+            if (down && left) return 7;
+            if (down && right) return 5;
+            if (up) return 2;
+            if (down) return 6;
+            if (left) return 8;
+            if (right) return 4;
+            return 1;
+        }
+
+        private static bool IsArrowKey(Keys key) =>
+            key == Keys.Up || key == Keys.Down || key == Keys.Left || key == Keys.Right;
+
+        private void EmergencyStop()
+        {
+            _heldArrowKeys.Clear();
+            _lastMotor = -1;
+            SendMotor(1);
+        }
+
+        private void SendMotor(int motorId)
+        {
+            SendEndpoint($"/motor/{motorId}", showMissingAddressMessage: false);
+        }
+
+        private void SendEndpoint(string endpoint, bool showMissingAddressMessage)
+        {
+            if (!TryGetBaseUrl(out var baseUrl, showMissingAddressMessage))
+                return;
+
+            if (string.IsNullOrWhiteSpace(endpoint))
+                return;
+
+            if (!endpoint.StartsWith("/"))
+                endpoint = "/" + endpoint;
+
+            RunCurlGet(baseUrl + endpoint);
+        }
+
+        private bool TryGetBaseUrl(out string baseUrl, bool showMissingAddressMessage)
+        {
+            if (!string.IsNullOrWhiteSpace(_baseUrl))
+            {
+                baseUrl = _baseUrl;
+                return true;
+            }
+
+            var normalized = NormalizeBaseUrl(textBox1.Text);
+            if (string.IsNullOrWhiteSpace(normalized))
+            {
+                if (showMissingAddressMessage)
+                    MessageBox.Show("Wpisz IP/adres w polu tekstowym.", "Brak adresu", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                baseUrl = string.Empty;
+                return false;
+            }
+
+            _baseUrl = normalized;
+            baseUrl = normalized;
+            return true;
+        }
+
+        private static string NormalizeBaseUrl(string input)
+        {
+            var s = (input ?? string.Empty).Trim();
+            if (string.IsNullOrWhiteSpace(s))
+                return string.Empty;
+
+            if (!s.Contains("://"))
+                s = "http://" + s;
+
+            return s.TrimEnd('/');
+        }
+
+        private static void RunCurlGet(string url)
+        {
+            Task.Run(() =>
+            {
+                try
+                {
+                    var psi = new ProcessStartInfo
+                    {
+                        FileName = "curl",
+                        Arguments = $"-s -m 2 -X GET \"{url}\"",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        CreateNoWindow = true
+                    };
+
+                    var p = Process.Start(psi);
+                    if (p == null)
+                        return;
+
+                    p.WaitForExit(2500);
+                }
+                catch
+                {
+                }
+            });
+        }
+    }
+}
